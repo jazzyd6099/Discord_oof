10fe97cacb8fdf017a44027ae2a9abbe1684cdac
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-13 02:08:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,15 +14,6 @@ client.on("message", (message) => {
     message.channel.send("Heh, mine's _");
   }
 });
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome, ${member}!`);
-});
 const prefix = "_";
 
 client.on("message", (message) => {

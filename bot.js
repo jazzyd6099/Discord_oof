4372514a9b5a897552c94ce1365f9ee160fe27d6
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-17 22:36:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('Yay..');
  
- client.user.setStatus("dnd")
+ client.user.setStatus("Invisible")
 
 });
 client.on("message", (message) => {
